72da2d8cee90572f22ea7d52a86a80d8f6283f0a
DiositoDiego <20213tn096@utez.edu.mx>
2023-03-17 17:37:40 -0600

    boton medio creado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MyNavbar from './componets/Navbar';
 import './App.css';
+import LoginButton from './componets/buttons/LoginButton';
 
 function App() {
     return (
       <div>
         <MyNavbar />
+        <LoginButton/>
       </div>
     );
 
---
+++ src/componets/buttons/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Button(props) {
+  return (
+    <>
+        <button style={(!props.style) ? styles.button : props.style}>{props.text}</button>
+    </>
+  )
+}
+
+const styles = {
+    button: {
+        backgroundColor: 'blue',
+        color: 'white',
+        borderRadius: '10px',
+        width: 10
+    }   
+}
