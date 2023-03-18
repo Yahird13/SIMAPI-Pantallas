004e8d31aca9e6a73b70ccd33b9f66688cc2e4d1
Unknow <20213tn102@utez.edu.mx>
2023-03-18 14:42:05 -0600

    primeros cambios login

---
+++ src/account/LoginForm.js
@@ -0,0 +1,18 @@
+import LoginButton from "../componets/buttons/LoginButton";
+
+
+export const LoginForm = () => {
+    return(
+        <div>
+            <div style={{padding: '20px'}}>
+                <text style={{fontWeight: 'bold', fontSize:'25px'}}>Bienvenido a SIMAPI</text>
+            </div>
+            <div>
+                
+            </div>
+            <div>
+                <LoginButton/>
+            </div>
+        </div>
+    );
+};
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { LoginForm } from "../account/LoginForm";
 import LoginButton from "../componets/buttons/LoginButton";
 
 export const LoginScreen = () => {
     return(
         <div>
-            <div style={{padding: '10px'}}>hola, esta es la prueba de la perra pantalla de login</div>
-            <LoginButton/>
+            <LoginForm/>
         </div>
     );
 };
