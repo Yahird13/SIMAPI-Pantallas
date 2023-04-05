edf4060e8c91c0d3fe17d7a0e9b979598958e16e
DiositoDiego <20213tn096@utez.edu.mx>
2023-04-04 20:28:20 -0600

    pinches conflictos pendejos

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,24 @@
-import React, {useEffect} from 'react';
-import SimapiNavbar from '../componets/navbar/SimapiNavbar'
-import { isUserAuthenticated } from '../auth/TokenValidate';
+import React from 'react';
+import SimapiNavbar from '../componets/navbar/SimapiNavbar';
+import CamillaContainer from '../componets/containers/CamillaContainer';
+import { Select } from '@material-ui/core';
 
 export default function HomeScreen() {
-    useEffect(() => {
-        if (!isUserAuthenticated()) {
-            window.location.href = "/";
-        }
-    }, []);
+
+    const camillas = [
+        { idCamilla: 'camilla1', paciente: 'Diego', estado: false, expediente: 'expediente 1' },
+        { idCamilla: 'camilla2', paciente: 'Johana', estado: true, expediente: 'expediente 2' },
+        { idCamilla: 'camilla3', paciente: 'Juan', estado: false, expediente: 'expediente 3' },
+        { idCamilla: 'camilla4', paciente: 'Pedro', estado: true, expediente: 'expediente 4' },
+        { idCamilla: 'camilla5', paciente: 'Maria', estado: false, expediente: 'expediente 5' },
+        { idCamilla: 'camilla6', paciente: 'Luis', estado: true, expediente: 'expediente 6' },
+        { idCamilla: 'camilla7', paciente: 'Luisa', estado: false, expediente: 'expediente 7' },
+        { idCamilla: 'camilla8', paciente: 'Antionio', estado: true, expediente: 'expediente 8' },
+        { idCamilla: 'camilla9', paciente: 'Marcos', estado: false, expediente: 'expediente 9' },
+        { idCamilla: 'camilla10', paciente: 'Fernando', estado: true, expediente: 'expediente 10' },
+    ]
+
+
     return (
         <div>
             <SimapiNavbar navbarItems={[
@@ -25,10 +36,28 @@ export default function HomeScreen() {
                 top: '175px',
                 borderRadius: '15px',
                 border: '5px solid black'
-            }}></div>
+            }}>
+                <select style={{
+                    marginTop:'20px',
+
+                }}>
+                    <option>Isla 1</option>
+                    <option>Isla 2</option>
+                </select>
+                <select>
+                    <option>Sala 1</option>
+                    <option>Sala 2</option>
+                    <option>Sala 3</option>
+                    <option>Sala 4</option>
+                    <option>Sala 5</option>
+                </select>
+                <CamillaContainer camillas={camillas} />
+
+            </div>
         </div>
 
 
 
     )
 }
+
